c0b19bc5f6eb4ea6e61d0a98f178d1d6e8b5a4b5
Islomjongit <islomjonbbbbb@gmail.com>
2023-10-25 20:20:37 +0500

    registasiyani yarimi

---
+++ registration.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <!-- link bootstrap css-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- link css -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container bg-light">
+        <?php
+        if (isset($_POST["submit"])) {
+            $fullName = $_POST["fullname"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $passwordRepeat = $_POST["repeat_password"];
+
+            $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+            $errors = array();
+
+            if (empty($fullName) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
+                array_push($errors, "All fields are required");
+            }
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                array_push($errors, "Email is not valid");
+            }
+
+            if(strlen($password)<8) {
+                array_push($errors, "Password must be at least 8 charactes long");
+            }
+
+            if ($password!==$passwordRepeat) {
+                array_push($errors, "Password does not match");
+            }
+
+            if (count($errors)>0) {
+                foreach ($errors as $error) {
+                    echo "<div class='alert alert-danger'>$error</div>";
+                }
+            } else {
+                require_once "database.php";
+
+                $sql = "INSERT INTO users (first_name, email, password) VALUES ( ?, ?, ? )";
+
+                $stmt = mysqli_stmt_init($conn);
+                $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
+
+                if ($prepareStmt) {
+                    mysqli_stmt_bind_param($stmt, "sss", $fullName, $email, $passwordHash);
+                    mysqli_stmt_execute($stmt);
+                    echo "<div class='alert alert-success'>You are registered successfully!</div>";
+                } else {
+                    die ("Something went wrong?");
+                }
+            }
+        }
+        ?>
+        <form action="registration.php" method="post">
+            <div class="form-group">
+                <input type="text" name="fullname" class="form-control" placeholder="Name">
+            </div>
+            <div class="form-group">
+                <input type="email" name="email" class="form-control" placeholder="Email:">
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" class="form-control" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <input type="text" name="repeat_password" class="form-control" placeholder="Repeat Password:">
+            </div>
+            <div class="form-btn text-center">
+                <input type="submit" class="btn btn-success" value="Register" name="submit">
+            </div>
+        </form>
+    </div>
+
+    <!-- link bootstrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
